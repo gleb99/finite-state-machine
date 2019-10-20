e03c379e8b7a42807972412e64c5e154b0a562e4
Gleb <gllukutin@gmail.com>
2019-10-20 20:34:55 +0300

    feat: add the solving of the task

--- src/fsm.js
+++ src/fsm.js
@@ -1,59 +1,125 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
+ /**
+   * Creates new FSM instance.
+   * @param config
+   */
+  constructor(config) {
+      this.state = config.initial;
+      this.history = ['normal'];
+      this.undoArr = [];
+  }
+  
+  /**
+   * Returns active state.
+   * @returns {String}
+   */
+  getState() {
+    return this.state = this.state;
+  }
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+  /**
+   * Goes to specified state.
+   * @param state
+   */
+  changeState(state) {
+    if (state === 'normal' || state === 'busy' || state === 'sleeping' || state === 'hungry') {
+      this.history.push(state);
+      return  this.state = state;
+      
+    } else { 
+      return this.state = 'Error';
+    }
+    
+  }
+  /**
+   * Changes state according to event transition rules.
+   * @param event
+   */
+  trigger(event) {
+    if (this.state === 'Error') {
+      return this.state = 'Error'
+    }
+    else if (event === 'study' || event === 'get_tired' || event === 'get_hungry' || event === 'eat' || event === 'get_up') {
+    let result = config.states[this.state].transitions[event];
+    this.history.push(result);
+    return this.state = result;
+    } else {
+      return this.state = 'Error';
+    }
+  }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+  /**
+   * Resets FSM state to initial.
+   */
+  reset() {
+    this.state = config.initial;
+  }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+  /**
+   * Returns an array of states for which there are specified event transition rules.
+   * Returns all states if argument is undefined.
+   * @param event
+   * @returns {Array}
+   */
+  getStates(event) {
+    if (!event) {
+      return ['normal', 'busy', 'hungry', 'sleeping'];
+    } else if (event === 'study' || event === 'get_tired' ||  event ==='get_hungry' ||  event ==='eat' ||  event ==='get_up') {
+      return Object.keys(config.states)
+.filter(state => config.states[state].transitions[event]);
+    } else if (event !== 'study' || event !== 'get_tired' || event !=='get_hungry' || event !=='eat' || event !=='get_up') {
+     return [];
+    }}
+    
+  /**
+   * Goes back to previous state.
+   * Returns false if undo is not available.
+   * @returns {Boolean}
+   */
+  undo() {
+    if ( this.history.length === 0) {
+      return false;
+    } else {
+      let popArr = this.history.pop();
+      this.state = this.history[this.history.length -1 ];
+     this.undoArr.push(popArr);
+     if ( this.history.length === 0) {
+       return false;
+     } else {
+     return true;
+      }
+    }
+  }
+  
+  /**
+   * Goes redo to state.
+   * Returns false if redo is not available.
+   * @returns {Boolean}
+   */
+  redo() {
+  if ( this.history.length === 0) {
+    return false;
+ } else if (this.state === this.history[this.history.length - 1]) {
+   return false;
+ } else {
+   if (this.undoArr.length === 0) {
+     return false;
+   } else {
+  let redoArr = this.undoArr.pop();
+  this.history.push(redoArr);
+  this.state = this.history[this.history.length -1 ];
+    return true;}
+     }
+  }
+  
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
-
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
-
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
-
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+  /**
+   * Clears transition history
+   */
+  clearHistory() {
+    return this.history = [];
+  }
 }
 
 module.exports = FSM;
